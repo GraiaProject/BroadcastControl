6c68cb2b1f6488e49ce892d7530c2f8f1872e5f5
GreyElaina <31543961+GreyElaina@users.noreply.github.com>
2020-07-24 19:45:31 +0800

    released 0.0.4

--- src/graia/broadcast/entities/event.py
+++ src/graia/broadcast/entities/event.py
@@ -1,23 +1,31 @@
-from abc import ABCMeta
-
 from iterwrapper import IterWrapper
-from pydantic import BaseModel  # pylint: disable=no-name-in-module
+from pydantic.main import ModelMetaclass
 
 from .dispatcher import BaseDispatcher
+import copy
 
+class EventMeta(ModelMetaclass):
+    class Config:
+        arbitrary_types_allowed = True
 
-class EventMeta(ABCMeta):
     def __new__(mcls, name, bases, mapping, **kwargs):
         if any(IterWrapper(bases).filter(lambda x: getattr(x, "__base_event__", False)).collect(list)):
             if not mapping.__contains__("__base_event__"):
                 mapping["__base_event__"] = False
         if not mapping.get("Dispatcher") and name != "BaseEvent":
             raise AttributeError("a event class must have a dispatcher called 'Dispatcher'")
-        return super().__new__(mcls, name, bases, mapping, **kwargs)
+        if not mapping.get("Config") or not getattr(mapping.get("Config"), "arbitrary_types_allowed", False):
+            if not mapping.get("Config"):
+                mapping['Config'] = copy.copy(mcls.Config)
+            else:
+                mapping['Config'].arbitrary_types_allowed = True
+        r = super().__new__(mcls, name, bases, mapping, **kwargs)
+        if mapping.get("type"):
+            r.type = mapping.get("type")
+        return r
 
 class BaseEvent(metaclass=EventMeta):
     Dispatcher: BaseDispatcher
 
     def __init__(self, **kwargs):
-        for k, v in kwargs.items():
-            setattr(self, k, v)
+        super().__init__(**kwargs)
