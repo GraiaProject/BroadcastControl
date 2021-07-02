4c96f98d2de53c87b5b40b0b64e974751ce755e4
GreyElaina <GreyElaina@outlook.com>
2021-07-02 22:00:31 +0800

    release 0.11.2

--- src/graia/broadcast/entities/event.py
+++ src/graia/broadcast/entities/event.py
@@ -4,24 +4,9 @@ from typing import TYPE_CHECKING, Any, Protocol, Tuple, TypeVar, Union
 if TYPE_CHECKING:
     from .dispatcher import BaseDispatcher
 
-T = TypeVar("T")
 
-
-def issubclass_safely(
-    cls, class_or_tuple: Union[type, Tuple[Union[type, Tuple], ...]]
-) -> bool:
-    return inspect.isclass(cls) and issubclass(cls, class_or_tuple)
-
-
-class Dispatchable(Protocol[T]):
-    def __instancecheck__(self, instance: Any) -> bool:
-        if issubclass_safely(getattr(instance, "Dispatcher", None), BaseDispatcher):
-            return True
-        return super().__instancecheck__(instance)
-
+class Dispatchable:
     Dispatcher: "BaseDispatcher"
 
 
 BaseEvent = Dispatchable
-
-from .dispatcher import BaseDispatcher
