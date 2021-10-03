b022dd85947a85c64ce7bdff1f31771345ee5518
ProgramRipper <programripper@foxmail.com>
2021-10-03 16:24:46 +0800

    Update entities\track_log.py
    Use relative import

--- src/graia/broadcast/entities/track_log.py
+++ src/graia/broadcast/entities/track_log.py
@@ -2,7 +2,7 @@ from enum import Enum
 from typing import TYPE_CHECKING, Any, List, Literal, Tuple, Union
 
 if TYPE_CHECKING:
-    from graia.broadcast.typing import T_Dispatcher
+    from ..typing import T_Dispatcher
 
 
 class TrackLogType(Enum):
