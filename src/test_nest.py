76031d148780419b748a9ffb4cf94f9d45e12a8c
Tarrailt <3165388245@qq.com>
2023-06-19 14:45:26 +0800

    :bug: fix nest error (#65)
    * :bug: fix nest error
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

---
+++ src/test_nest.py
@@ -0,0 +1,71 @@
+import asyncio
+
+from graia.broadcast import Broadcast
+from graia.broadcast.builtin.decorators import Depend
+from graia.broadcast.entities.dispatcher import BaseDispatcher
+from graia.broadcast.entities.event import Dispatchable
+from graia.broadcast.interfaces.dispatcher import DispatcherInterface
+
+
+class TestDispatcher(BaseDispatcher):
+    @classmethod
+    async def beforeExecution(cls, interface: DispatcherInterface):
+        print("beforeExecution")
+
+    @classmethod
+    async def catch(cls, interface: DispatcherInterface):
+        if interface.name == "ster":
+            return 1
+
+
+class TestEvent(Dispatchable):
+    class Dispatcher(BaseDispatcher):
+        mixin = [TestDispatcher]
+
+        @classmethod
+        async def catch(cls, interface: "DispatcherInterface"):
+            if interface.name == "ster1":
+                return "1"
+            elif interface.name == "ster2":
+                return 54352345
+
+        @classmethod
+        async def afterExecution(
+            self,
+            interface: DispatcherInterface,
+            *args,
+        ):
+            print("afterExecution")
+
+
+event = TestEvent()
+loop = asyncio.new_event_loop()
+
+broadcast = Broadcast(
+    loop=loop,
+    debug_flag=False,
+)
+
+
+@broadcast.receiver(
+    TestEvent,
+    decorators=[
+        Depend(lambda ster: print("depend: ster", ster)),
+        Depend(lambda ster1: print("depend: ster1", ster1)),
+        Depend(lambda ster2: print("depend: ster2", ster2)),
+    ],
+)
+async def s(e: TestEvent):
+    print(e)
+
+
+def error(ster: int):
+    raise ValueError("error", ster)
+
+
+@broadcast.receiver(TestEvent, decorators=[Depend(error)])
+async def s1(e: TestEvent):
+    print(e)
+
+
+loop.run_until_complete(asyncio.wait([broadcast.postEvent(event)]))
